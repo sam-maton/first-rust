bbded5cda46d9d23a60a391dfaf781c39fda1550
smaton1 <sam.maton91@gmail.com>
2023-02-13 16:47:31 +0000

    Guessing game
    - added some color

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use colored::*;
 use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     //Guessing Game
-    let number = thread_rng().gen_range(0..10);
+    let number = thread_rng().gen_range(0..100);
 
     loop {
         println!("Enter your guess");
@@ -18,18 +19,18 @@ fn main() {
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("Please enter a number");
+                println!("{}", "Please enter a number".red());
                 continue;
             }
         };
 
-        println!("Your guessed: {} ", guess);
+        println!("Your guessed: {} ", guess.to_string().blue());
 
         match guess.cmp(&number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("{}", "Too small!".bold()),
+            Ordering::Greater => println!("{}", "Too big!".bold()),
             Ordering::Equal => {
-                println!("You win!");
+                println!("{}", "You win!".green().bold());
                 break;
             }
         }
