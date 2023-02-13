48675bbe9ae631459a919712eda875bb6538a35e
smaton1 <sam.maton91@gmail.com>
2023-02-13 16:35:51 +0000

    Guessing game
    - Finished basic guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    //Guessing Game
+    let number = thread_rng().gen_range(0..10);
+
+    loop {
+        println!("Enter your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Could not read guess");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
+
+        println!("Your guessed: {} ", guess);
+
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
